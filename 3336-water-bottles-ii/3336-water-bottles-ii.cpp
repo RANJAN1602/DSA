f64cccda2e509b70fb4acd94ec2bb566e1115f4d
RANJAN1602 <kranjan36572@gmail.com>
2025-10-03 18:44:12 +0530

    Time: 0 ms (100%), Space: 8.5 MB (79.3%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int totalDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange;
+            numExchange++;
+            totalDrunk++;
+            emptyBottles++;
+        }
+
+        return totalDrunk;
+    }
+};
