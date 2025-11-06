40298e5c2f1b97db47e8708a0075bb2cf1de8ba5
RANJAN1602 <kranjan36572@gmail.com>
2025-11-06 23:56:18 +0530

    Time: 3 ms (59.42%), Space: 11.9 MB (50.48%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> charCounts;
+        
+        for (char c : magazine) {
+            charCounts[c]++;
+        }
+        
+        for (char c : ransomNote) {
+            if (charCounts[c] > 0) {
+                charCounts[c]--;
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
