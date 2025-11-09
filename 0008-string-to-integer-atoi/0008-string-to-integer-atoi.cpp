aff1f036a4d92312583e48eb316ea5ca5990d4a1
RANJAN1602 <kranjan36572@gmail.com>
2025-11-10 00:00:46 +0530

    Time: 4 ms (6.26%), Space: 9 MB (97.32%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.length();
+        
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+        
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+        
+        long long num = 0;
+        while (i < n && isdigit(s[i])) {
+            num = num * 10 + (s[i] - '0');
+            
+            if (sign == 1 && num > INT_MAX) return INT_MAX;
+            if (sign == -1 && -num < INT_MIN) return INT_MIN;
+            
+            i++;
+        }
+        
+        return (int)(num * sign);
+    }
+};
